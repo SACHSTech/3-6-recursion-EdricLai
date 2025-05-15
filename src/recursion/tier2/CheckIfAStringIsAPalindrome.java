62de18c9fd76a7b1fe3498d818a4823a61090dcc
EdricLai <edric.lai25@ycdsbk12.ca>
2025-05-14 22:32:31 -0400

    finished isPalindrome

--- src/recursion/tier2/CheckIfAStringIsAPalindrome.java
+++ src/recursion/tier2/CheckIfAStringIsAPalindrome.java
@@ -8,9 +8,20 @@ public class CheckIfAStringIsAPalindrome {
      * @return true/false if s is a palindrome
     */
     public static boolean isPalindrome(String s) {
-        return true;
-    }
+        // base case
+        if (s.length() <= 1) {
+            return true;
+        }
 
+        // check first and last char if equal
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        // recursive case: each itt loses first and last char
+        return isPalindrome(s.substring(1, s.length() - 1));
+    }
+    
     // testing
     public static void main(String[] args) {
         System.out.println(isPalindrome("wow")); // Output: true
