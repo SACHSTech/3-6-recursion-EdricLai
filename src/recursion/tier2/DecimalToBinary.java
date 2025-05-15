64807b4864ee07a6685d5b062094d2ca319720b4
EdricLai <edric.lai25@ycdsbk12.ca>
2025-05-15 00:50:09 -0400

    finished toBinary

--- src/recursion/tier2/DecimalToBinary.java
+++ src/recursion/tier2/DecimalToBinary.java
@@ -9,11 +9,31 @@ public class DecimalToBinary {
      * @return binary representation of n
     */
     public static String toBinary(int n) {
-        return null;
-    }
+        // base case
+        if (n < 0) {
+            return "";
+        }
+        if (n == 0) {
+            return "0";
+        }
+        if (n == 1) {
+            return "1";
+        }
 
+        /*
+        recursive case:
+            divide n by 2 to shift to the next power of 2
+            n % 2 to get the last binary digit
+            each call builds the binary string
+        */ 
+        return toBinary(n / 2) + Integer.toString(n % 2);
+    }
+    
     // testing
     public static void main(String[] args) {
         System.out.println(toBinary(10)); // Output: 1010
+        System.out.println(toBinary(0)); // Output: 0
+        System.out.println(toBinary(254)); // Output: 11111110
+        System.out.println(toBinary(1284)); // Output: 10100000100
     }
 }
